23c25774868c80b64beb7cb530a48f6100125f81
kSneha12 <sneha.kodde@gmail.com>
2018-02-10 17:08:56 +0530

    newpro

---
+++ app/src/main/java/sneha/example/lenovo/gitapp/GActivity.java
@@ -0,0 +1,14 @@
+package sneha.example.lenovo.gitapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GActivity extends AppCompatActivity {
+    Button button;
+    ImageView view;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_g);
+    }
+}
